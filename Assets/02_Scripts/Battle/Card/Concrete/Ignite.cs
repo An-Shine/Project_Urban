50e9ddae319df41e779ef634bb2cbd08c212ee9d
An-Shine <akh1012@naver.com>
2025-11-28 15:14:15 +0900

    Update Ignite.cs

--- Assets/02_Scripts/Battle/Card/Concrete/Ignite.cs
+++ Assets/02_Scripts/Battle/Card/Concrete/Ignite.cs
@@ -1,21 +1,22 @@
 using UnityEngine;
 
-public class Ignite : Card 
+
+public class Ignite : Attack
 {   
-    [SerializeField] private int initialDamage = 1; // 즉발 데미지
-    [SerializeField] private int tickDamage = 1;    // 턴당 데미지
-    [SerializeField] private int duration = 3;      // 지속 시간
+    [SerializeField] private int burnDamage = 1; // 도트 데미지
+    [SerializeField] private int duration = 3;   // 지속 시간
+
     
     public override CardName Name => CardName.Ignite;
 
     public override int Use(Target target)
     {
-        // 즉발 데미지
-        target.Damage(initialDamage);
+        // 1. 즉발 데미지 
+        target.Damage(damage);
 
-        // 도트데미지 예약 
-        target.AddDoT(tickDamage, duration);       
-
+        // 2. 도트 데미지 
+        target.AddDoT(burnDamage, duration);
+        
         return cost;
     }
 }
